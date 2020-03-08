6bfe9ae4f0ebc93416562ecdaa8ce32c91b4e92d
AnnaTerelius <tereliusanna@hotmail.com>
2020-03-08 13:54:01 +0100

    added personalsettings

--- frontend/src/components/Counter.js
+++ frontend/src/components/Counter.js
@@ -1,11 +1,31 @@
 import React, {useState, useEffect} from 'react';
 
 export const Counter = (props) => {
-    const personalSetting = 12
+    //500/total daily dos insulin = 500/35= 14.3 (1 unit insulin takes care of 14.3 g carbs)
+    const personalSetting = 14.3
+    const goalValue = 5
+    //1 unit insulin reduces the bloodsugarlevel with 2.5 mmol/L
+    const reductionPerUnit = 2.5
     const [bloodSugarLevel, setBloodSugarLevel] = useState('')
+   
     
-   
-   
+   //calculate total carbs of selected products
+        let totalCarbs = 0
+        props.text.map(item => { 
+            console.log('maping selected carbs value:')
+            totalCarbs = totalCarbs+ JSON.parse(item).filter(({name}) => name === 'Kolhydrater')[0].value;
+            console.log(totalCarbs)
+      })
+
+      //calculate dose for selected products
+      let insulinDose = totalCarbs/personalSetting
+console.log('dose only selected products carbs' + insulinDose)
+      //adjust for current bloodsugarlevel
+      insulinDose += (bloodSugarLevel-goalValue)/reductionPerUnit
+      console.log('dose selected products carbs + reduction if high bloodsugarlevel' + insulinDose)
+
+
+      
 
    const  handleSubmit = (event) => {
     event.preventDefault()
@@ -33,7 +53,8 @@ return (
                 <article className="inputField">
                    
                     personal dose:  {personalSetting}<br/>
-                    my current bloodsugarlevel:  {bloodSugarLevel}
+                    my current bloodsugarlevel:  {bloodSugarLevel}<br/>
+                    insulin dose:  {insulinDose}
 
                     <input type="text" placeholder=" current bloodsugarlevel" value={bloodSugarLevel} required className="product" onChange = {(event) => { setBloodSugarLevel(event.target.value); console.log("event onChange: Texten är: " + event.target.value)}}/>
 
@@ -41,23 +62,10 @@ return (
                     <button className="submit-btn" type="reset">reset</button>
                 
                 </article>
-                <ul>
-       
-       {props.text.map(item => { 
-           console.log('Found a selected product:')
-           const carbs = JSON.parse(item).filter(({name}) => name === 'Kolhydrater');
-           console.log(carbs)
-         
-           
-           return <ul key={item.id}> carbs of selected products: <br/> {carbs[0].value}</ul>
-          
-           
-         })
-       }
-      
-     </ul>
+        
             </form>
-                
+             {totalCarbs}
+            
         </div> 
     </>
   )
